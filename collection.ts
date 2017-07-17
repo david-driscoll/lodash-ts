645bbc83f0ad91248448118c3499fe1eab6c9aed
David Driscoll <david.driscoll@gmail.com>
2017-07-17 11:59:15 -0400

    fixed some typings for objects

--- collection.ts
+++ collection.ts
@@ -1,12 +1,12 @@
 namespace Types {
     export interface CountBy {
         <T, R extends string>(collection: ArrayLike<T>, iteratee: ValuePredicate<T, R>): { [ P in R ]: number; };
-        <T, R extends string>(collection: T, iteratee: (value: ValuePredicate<T[keyof T], R>) => R): { [ P in R ]: number; };
+        <T extends object, R extends string, K extends keyof T>(collection: T, iteratee: (value: ValuePredicate<T[K], R>) => R): { [ P in R ]: number; };
     }
 
     export interface GroupBy {
         <T, R extends string>(collection: ArrayLike<T>, iteratee: ValuePredicate<T, R>): { [ P in R ]: T[]; };
-        <T, R extends string>(collection: T, iteratee: ValuePredicate<T[keyof T], R>): { [ P in R ]: T[]; };
+        <T extends object, R extends string, K extends keyof T>(collection: T, iteratee: ValuePredicate<T[K], R>): { [ P in R ]: T[K][]; };
     }
 
     export interface ByBooleanPredicate {
@@ -15,11 +15,11 @@ namespace Types {
         <T>(collection: ArrayLike<T>, iteratee: Property<T>): boolean;
         <T>(collection: ArrayLike<T>, iteratee: Matches<T>): boolean;
         <T, K extends keyof T>(collection: ArrayLike<T>, iteratee: MatchesProperty<T, K>): boolean;
-        <T>(collection: T): boolean;
-        <T, K extends keyof T>(collection: T, iteratee: ObjectPredicate<T, K>): boolean;
-        <T>(collection: T, iteratee: Property<T>): boolean;
-        <T>(collection: T, iteratee: Matches<T[keyof T]>): boolean;
-        <T, K extends keyof T>(collection: T, iteratee: MatchesProperty<T, K>): boolean;
+        <T extends object>(collection: T): boolean;
+        <T extends object, K extends keyof T>(collection: T, iteratee: ObjectPredicate<T, K>): boolean;
+        <T extends object>(collection: T, iteratee: Property<T>): boolean;
+        // <T extends object>(collection: T, iteratee: Matches<T[keyof T]>): boolean;
+        <T extends object, K extends keyof T>(collection: T, iteratee: MatchesProperty<T, K>): boolean;
     }
 
     export interface ByArrayPredicate {
@@ -27,10 +27,10 @@ namespace Types {
         <T>(collection: ArrayLike<T>, iteratee: Property<T>): T[];
         <T>(collection: ArrayLike<T>, iteratee: Matches<T>): T[];
         <T, K extends keyof T>(collection: ArrayLike<T>, iteratee: MatchesProperty<T, K>): T[];
-        <T, K extends keyof T>(collection: T, iteratee: ObjectPredicate<T, K>): T[];
-        <T>(collection: T, iteratee: Property<T>): T[];
-        <T>(collection: T, iteratee: Matches<T>): T[];
-        <T, K extends keyof T>(collection: T, iteratee: MatchesProperty<T, K>): T[];
+        <T extends object, K extends keyof T>(collection: T, iteratee: ObjectPredicate<T, K>): T[K][];
+        <T extends object, K extends Property<T>>(collection: T, iteratee: K): T[K][];
+        // <T extends object, K extends keyof T, M extends Pick<T, K>>(collection: T, iteratee: M): T[K][];
+        <T extends object, K extends keyof T>(collection: T, iteratee: MatchesProperty<T, K>): T[K][];
     }
 
     export interface FindPredicate {
@@ -38,10 +38,10 @@ namespace Types {
         <T>(collection: ArrayLike<T>, iteratee: Property<T>, fromIndex?: number): T;
         <T>(collection: ArrayLike<T>, iteratee: Matches<T>, fromIndex?: number): T;
         <T, K extends keyof T>(collection: ArrayLike<T>, iteratee: MatchesProperty<T, K>, fromIndex?: number): T;
-        <T, K extends keyof T>(collection: T, iteratee: ObjectPredicate<T, K>, fromIndex?: number): T;
-        <T>(collection: T, iteratee: Property<T>, fromIndex?: number): T;
-        <T>(collection: T, iteratee: Matches<T>, fromIndex?: number): T;
-        <T, K extends keyof T>(collection: T, iteratee: MatchesProperty<T, K>, fromIndex?: number): T;
+        <T extends object, K extends keyof T>(collection: T, iteratee: ObjectPredicate<T, K>, fromIndex?: number): T[K];
+        <T extends object, K extends Property<T>>(collection: T, iteratee: Property<T>, fromIndex?: number): T[K];
+        // <T>(collection: T, iteratee: Matches<T>, fromIndex?: number): T;
+        <T extends object, K extends keyof T>(collection: T, iteratee: MatchesProperty<T, K>, fromIndex?: number): T[K];
     }
 
     export type FlatMapIteratorArray<T, R> = (value: T, index: number, collection: T[]) => R[];
@@ -50,13 +50,13 @@ namespace Types {
     export interface FlatMap {
         <T>(collection: ArrayLike<ArrayLike<T>>): T[];
         <T, R>(collection: ArrayLike<T>, iteratee: FlatMapIteratorArray<T, R>): R[];
-        <T, R>(collection: T, iteratee: FlatMapIteratorObject<T, R>): R[];
+        <T extends object, R>(collection: T, iteratee: FlatMapIteratorObject<T, R>): R[];
     }
 
     export interface FlatMapDepth {
         <T>(collection: ArrayLike<ArrayLike<T>>, depth?: number): T[];
         <T, R>(collection: ArrayLike<T>, iteratee: FlatMapIteratorArray<T, R>, depth?: number): R[];
-        <T, R>(collection: T, iteratee: FlatMapIteratorObject<T, R>, depth?: number): R[];
+        <T extends object, R>(collection: T, iteratee: FlatMapIteratorObject<T, R>, depth?: number): R[];
     }
 
     export type ForEachIteratorArray<T> = (value: T, index: number, collection: Array<T>) => boolean | void;
@@ -64,13 +64,13 @@ namespace Types {
 
     export interface ForEach {
         <T>(collection: ArrayLike<T>, iteratee: ForEachIteratorArray<T>): ArrayLike<T>;
-        <T>(collection: T, iteratee: ForEachIteratorObject<T>): T;
+        <T extends object>(collection: T, iteratee: ForEachIteratorObject<T>): T;
     }
 
     export interface Includes {
         (collection: string, value: string, fromIndex?: number): boolean;
         <T>(collection: ArrayLike<T>, value: T, fromIndex?: number): boolean;
-        <T>(collection: T, value: any, fromIndex?: number): boolean;
+        <T extends object, K extends Property<T>>(collection: T, value: T[K], fromIndex?: number): boolean;
     }
 
     export interface InvokeMap {
@@ -98,19 +98,19 @@ namespace Types {
     export interface _Map {
         <T, R>(collection: ArrayLike<T>, iteratee: MapIteratorArray<T, R>): R[];
         <T, K extends keyof T>(collection: ArrayLike<T>, iteratee: K): T[K][];
-        <T, R, K extends keyof T>(collection: T, iteratee: MapIteratorObject<T, R, K>): R[];
+        <T extends object, R, K extends keyof T>(collection: T, iteratee: MapIteratorObject<T, R, K>): R[];
     }
 
     export interface OrderBy {
         <T, R>(collection: ArrayLike<T>, iteratee: ValuePredicate<T, R>, orders?: ("asc" | "desc") | ("asc" | "desc")[]): T[];
         <T, R>(collection: ArrayLike<T>, iteratee: ValuePredicate<T, R>[], orders?: ("asc" | "desc") | ("asc" | "desc")[]): T[];
-        <T, R>(collection: T, iteratee: ValuePredicate<T, R>, orders?: ("asc" | "desc") | ("asc" | "desc")[]): T[];
-        <T, R>(collection: T, iteratee: ValuePredicate<T, R>[], orders?: ("asc" | "desc") | ("asc" | "desc")[]): T[];
+        <T extends object, R, K extends keyof T>(collection: T, iteratee: ValuePredicate<T, R>, orders?: ("asc" | "desc") | ("asc" | "desc")[]): T[K][];
+        <T extends object, R, K extends keyof T>(collection: T, iteratee: ValuePredicate<T, R>[], orders?: ("asc" | "desc") | ("asc" | "desc")[]): T[K][];
     }
 
     export interface Partition {
         <T>(collection: ArrayLike<T>, iteratee: BooleanPredicate<T>): [T[], T[]];
-        <T>(collection: T, iteratee: BooleanPredicate<T>): [T[], T[]];
+        <T extends object, K extends keyof T>(collection: T, iteratee: BooleanPredicate<T>): [T[K][], T[K][]];
     }
 
     export interface Reduce {
@@ -139,10 +139,10 @@ namespace Types {
         <T, R>(collection: ArrayLike<T>, iteratee: ValuePredicate<T, R>[]): T[];
         <T>(collection: ArrayLike<T>, ...iteratees: ValuePredicate<T, any>[]): T[];
         <T, R>(collection: ArrayLike<T>, ...iteratees: ValuePredicate<T, R>[]): T[];
-        <T>(collection: T, iteratee: ValuePredicate<T, any>[]): T[];
-        <T, R>(collection: T, iteratee: ValuePredicate<T, R>[]): T[];
-        <T>(collection: T, ...iteratees: ValuePredicate<T, any>[]): T[];
-        <T, R>(collection: T, ...iteratees: ValuePredicate<T, R>[]): T[];
+        <T extends object, K extends keyof T>(collection: T, iteratee: ValuePredicate<T[K], any>[]): T[K][];
+        <T extends object, R, K extends keyof T>(collection: T, iteratee: ValuePredicate<T[K], R>[]): T[K][];
+        <T extends object, K extends keyof T>(collection: T, ...iteratees: ValuePredicate<T[K], any>[]): T[K][];
+        <T extends object, R, K extends keyof T>(collection: T, ...iteratees: ValuePredicate<T[K], R>[]): T[K][];
     }
 }
 
